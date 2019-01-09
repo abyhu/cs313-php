568e5e47e0d67945765d58f8c33533644c55f605
amarro <abyhunter@gmail.com>
2019-01-08 22:12:55 -0700

    Assignment complete including stretch goals.

--- web/02TeamActivity/scripts/scripts.js
+++ web/02TeamActivity/scripts/scripts.js
@@ -1,8 +1,20 @@
 function buttonClick() {
-    var alert = this.document.getElementById("alertMessage");
+    var alert = document.getElementById("alertMessage");
     if (alert.style.display === "none") {
         alert.style.display = "block";
     } else {
         alert.style.display = "none";
     }
 }
+
+function changeColor() {
+    //var color = this.document.getElementById("colorInput").value;
+    //this.document.getElementById("one").style.backgroundColor = color;
+    $("#one").css("background-color", $("#colorInput").val());
+}
+
+$(document).ready(function (){
+   $("#toggleButton").click(function(){
+        $("#two").fadeOut("slow");
+    });
+});
