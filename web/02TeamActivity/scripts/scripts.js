551feac1683a1bb02cf8edb42444cd4eb60cff72
amarro <abyhunter@gmail.com>
2019-01-08 17:38:21 -0700

    Added alert box

---
+++ web/02TeamActivity/scripts/scripts.js
@@ -0,0 +1,8 @@
+function buttonClick() {
+    var alert = document.getElementById("alertMessage");
+    if (alert.style.display === "none") {
+        alert.style.display = "block";
+    } else {
+        alert.style.display = "none";
+    }
+}
