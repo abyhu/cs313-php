978d3d41576b64b255324cac9060fddcc055bfea
amarro <abyhunter@gmail.com>
2019-01-09 17:22:50 -0700

    Start style of Assignments Page

--- web/myHomepage/myAssignments.php
+++ web/myHomepage/myAssignments.php
@@ -52,7 +52,9 @@
 	  </footer>   
 	<!-- Optional JavaScript -->
     <!-- jQuery first -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 	<script src="scripts/scripts.js"></script>
   </body>
 </html>
