c41c1bc8f6f3d8f3379e19c5237c6c515599dd67
amarro <abyhunter@gmail.com>
2019-01-09 17:38:15 -0700

    Start style of Assignments Page

--- web/myHomepage/scripts/scripts.js
+++ web/myHomepage/scripts/scripts.js
@@ -1,5 +1,7 @@
 function onPageLoad() {
-	$("#pOne").fadeOut(5000);
+	$("#pOne").fadeIn(5000);
+	$("#pTwo").delay(1000).fadeIn(5000);
+	$("#pThree").delay(2000).fadeIn(5000);
 }
 	
 $(document).ready(onPageLoad);
