b8077ad574146e36c735409b61b29fbebed0133d
amarro <abyhunter@gmail.com>
2019-01-17 13:01:09 -0700

    Updates to shoppingCart creating PHP Modules

--- web/shoppingCart/scripts/navigation.js
+++ web/shoppingCart/scripts/navigation.js
@@ -8,39 +8,6 @@ function activateNavItem(navItem) {
     //set the selected id to an active attribute
     navItem.setAttribute("id", "active");
     var itemClass = navItem.classList[1];
-
-    //hide and display appropriate content for the selected nav item
-    if (itemClass == "0") {
-        document.title = "Pursula Home";
-        document.getElementById("index").style.display = "block";
-        document.getElementById("shop").style.display = "none";
-        document.getElementById("references").style.display = "none";
-        document.getElementById("checkout").style.display = "none";
-        resetForm();
-    } else if (itemClass == "1") {
-        document.title = "Pursula Shop";
-        document.getElementById("index").style.display = "none";
-        document.getElementById("shop").style.display = "block";
-        document.getElementById("references").style.display = "none";
-        document.getElementById("checkout").style.display = "none";
-    } else if (itemClass == "2") {
-        document.title = "Pursula References";
-        document.getElementById("index").style.display = "none";
-        document.getElementById("shop").style.display = "none";
-        document.getElementById("references").style.display = "block";
-        document.getElementById("checkout").style.display = "none";
-    } else if (itemClass == "3") {
-        navItem.href = "http://www.facebook.com";
-    } else if (itemClass == "4") {
-        navItem.href = "http://www.gmail.com";
-    } else {
-        console.log("error");
-    }
-
-    //keep the user at the top of the new page and clear error messages
-    window.scrollTo(0, 0);
-    document.getElementById("message").innerHTML = "";
-    document.getElementById("message").style.display = "block";
 }
 
 
