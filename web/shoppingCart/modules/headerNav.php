0dd24eaa2870724a63da3bd818bba92da221b697
amarro <abyhunter@gmail.com>
2019-01-17 13:03:40 -0700

    Updates to shoppingCart creating PHP Modules

--- web/shoppingCart/modules/headerNav.php
+++ web/shoppingCart/modules/headerNav.php
@@ -4,10 +4,10 @@
           	<a href="index.html"><img src="images/pursulaLogo.jpg" alt="Pursula Logo" id="logo" /></a>
 		</header>
 		<nav>
-      		<a href="index.php" class="nav 0">Home</a>
-      		<a href="shop.php" class="nav 1">Shop</a>
-      		<a href="references.php" class="nav 2">References</a>
-      		<a href="https://www.facebook.com" class="nav 3">Facebook</a>
-      		<a href="https://www.gmail.com" class="nav 4">Email</a>
+      		<a href="index.php" class="nav">Home</a>
+      		<a href="shop.php" class="nav">Shop</a>
+      		<a href="references.php" class="nav">References</a>
+      		<a href="https://www.facebook.com" class="nav">Facebook</a>
+      		<a href="https://www.gmail.com" class="nav">Email</a>
 		</nav>
 <main>
