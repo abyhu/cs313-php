7ce06f5bd025e3b52356afd2e055e5ccc55fc082
amarro <abyhunter@gmail.com>
2019-01-18 20:19:49 -0700

    Starting Shopping Cart

--- web/shoppingCart/checkout.php
+++ web/shoppingCart/checkout.php
@@ -56,9 +56,8 @@ session_start();
                             <span id=itemsOrdered>
 								
 							<?php 
-	   							echo "<h3> PHP List All Session Variables</h3>";
-    							foreach ($_SESSION as $key=>$val)
-    							echo $key." ".$val."<br/>";
+	   							foreach ($_SESSION['items'] as $key=>$val)
+    								echo $key." ".$val."<br/>";
 							?>
 							
 							</span>
