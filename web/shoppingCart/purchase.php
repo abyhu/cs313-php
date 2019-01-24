79b0522c75bf40d4d112584e95528dc89cf891c0
amarro <abyhunter@gmail.com>
2019-01-24 13:53:04 -0700

    Starting Checkout

--- web/shoppingCart/purchase.php
+++ web/shoppingCart/purchase.php
@@ -35,7 +35,7 @@ include("modules/headerNav.php");
 
 print " 
 		<div class='purchase'>
-		<p id='finalMessage'>Thank you for shopping at Pursula. Here is your receipt.</p>
+		<p id='finalMessage'>Thank you for shopping at Pursula. Please print out this page for your receipt. Your items will be arriving soon.</p>
 		<h3 class='review'>Shipping Information:</h3>
         <p>$fName $lName</p>
         <p>$street</p>
