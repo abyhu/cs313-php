978a0a73dc230935532271929548bf11288825d1
amarro <abyhunter@gmail.com>
2019-01-24 12:19:21 -0700

    Starting Checkout

--- web/shoppingCart/scripts/purchase.php
+++ web/shoppingCart/scripts/purchase.php
@@ -7,34 +7,20 @@ if(session_id() == '') {
 $fName = $lName = $street = $city = $state = $zip = $phone = $subtotal = $shipping = $tax = $total = $radio = $expiration = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$fName = $_POST["fName"];
-	$lName = $_POST["lName"];
-	$street = $_POST["street"];
-	$city = $_POST["city"];
-	$state = $_POST["state"];
-	$zip = $_POST["zip"];
-	$phone = $_POST["phone"];
-	$subtotal = $_POST["subtotal"];
-	$shipping = $_POST["shipping"];
-	$tax = $_POST["tax"];
-	$total = $_POST["total"];
-	$radio = $_POST["radio"];
-	$expiration = $_POST["expirationDate"];
+	$fName = $_SESSION["fName"];
+	$lName = $_SESSION["lName"];
+	$street = $_SESSION["street"];
+	$city = $_SESSION["city"];
+	$state = $_SESSION["state"];
+	$zip = $_SESSION["zip"];
+	$phone = $_SESSION["phone"];
+	$subtotal = $_SESSION["subtotal"];
+	$shipping = $_SESSION["shipping"];
+	$tax = $_SESSION["tax"];
+	$total = $_SESSION["total"];
+	$radio = $_SESSION["radio"];
+	$expiration = $_SESSION["expiration"];
 }
-
-$_SESSION["fName"] = $fName;
-$_SESSION["lName"] = $lName;
-$_SESSION["street"] = $street;
-$_SESSION["city"] = $city;
-$_SESSION["state"] = $state;
-$_SESSION["zip"] = $zip;
-$_SESSION["phone"] = $phone;
-$_SESSION["subtotal"] = $subtotal;
-$_SESSION["shipping"] = $shipping;
-$_SESSION["tax"] = $tax;
-$_SESSION["total"] = $total;
-$_SESSION["radio"] = $radio;
-$_SESSION["expiration"] = $expiration;
 
 print '<!DOCTYPE html>
 <html lang="en">
@@ -56,22 +42,21 @@ print " <h3 class=\"review\">Shipping Information:</h3>
         <p>$phone</p>
 
         <h3 class=\"review\">Order Summary:</h3>
-        <p>Your purchase includes: $itemsOrdered</p>
-        <p>Subtotal: $subtotal</p>
+        <p>Your purchase includes:"
+		
+		foreach ($_SESSION['items'] as $key=>$val) {
+			echo '<p>'; 
+			echo $key." <br/>";
+									
+print " <p>Subtotal: $subtotal</p>
         <p>Shipping Cost: $shipping</p>
         <p>Tax Charges: $tax</p>
         <p>Total Purchase: $total</p>
 
         <h3 class=\"review\">Payment Information:</h3>
         <p>Credit Card Type: $radio </p>
-        <p>Card Expiration Date: $expiration</p>";
-
-print ' <p>
-            <input type="submit" id="clear" name="clearForm" value="Cancel Order" onsubmit="return confirmAndPost(this)" />
-            <input type="submit" id="submitButton" name="submitForm" value="Submit Order" onsubmit="return confirmAndPost(this)" />
-        </p>
-    
+        <p>Card Expiration Date: $expiration</p>
+		
 	<!--INCLUDE FOOTER-->
-	<?php include("modules/footer.php");?>';
-
+	<?php include("modules/footer.php");?>";
 ?>
