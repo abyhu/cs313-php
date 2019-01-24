802653420e8816780d0fb603dddc059c3472b657
amarro <abyhunter@gmail.com>
2019-01-24 09:30:39 -0700

    Starting Checkout

--- web/shoppingCart/scripts/removeCart.php
+++ web/shoppingCart/scripts/removeCart.php
@@ -7,5 +7,7 @@ if(session_id() == '') {
 if(isset($_SESSION['items'][$_POST["id"]])) {
 	unset($_SESSION['items'][$_POST["id"]]);
 } 
+if($_SESSION['items'].sizeof() == 0)
+	unset($_SESSION['items']); 
 	  
 ?>
