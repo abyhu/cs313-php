017e999d42ffa2876f22829d8b101cb93146434a
amarro <abyhunter@gmail.com>
2019-01-24 11:57:39 -0700

    Starting Checkout

--- web/shoppingCart/checkout.php
+++ web/shoppingCart/checkout.php
@@ -38,7 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$_SESSION["radio"] = $radio;
 	$_SESSION["expiration"] = $expiration;
 	
-	window.open('purchase.php','_self',false);
+	//window.open('purchase.php','_self',false);
 	
 }
 
