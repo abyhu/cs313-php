8c2a5ac8dccc0b3506de02d02ac9e76822479d1c
amarro <abyhunter@gmail.com>
2019-01-24 08:17:12 -0700

    Starting Checkout

---
+++ web/shoppingCart/scripts/shop.js
@@ -0,0 +1,22 @@
+/* ___________Add Cart____________ */
+function addCart(shopItem) {
+	var itemId = {id: shopItem.id};  
+		//toggle between addCart and inCart classes on each click
+   		if (shopItem.className == "addCart") {
+       		shopItem.className = "inCart";
+			shopItem.value = "In Your Cart";
+			$.post('scripts/addToCart.php', itemId); 
+   		} else if (shopItem.className = "inCart") {
+     		shopItem.className = "addCart";
+			shopItem.value = "Add To Cart";
+			$.post('scripts/removeCart.php', itemId); 
+		}
+}
+
+/* ___________Open Checkout____________ */
+function openCheckout() {
+	window.open('checkout.php','_self',false)
+}
+		
+		
+	
--- web/shoppingCart/shop.php
+++ web/shoppingCart/shop.php
@@ -145,30 +145,7 @@ if(!isset($_SESSION['items'])) {
             </div>
 	
 	<!--ADD FUNCTIONS-->
-	
-	<script> 
-		
-		/* ___________Add Cart____________ */
-		function addCart(shopItem) {
-			var itemId = {id: shopItem.id};  
-    		//toggle between addCart and inCart classes on each click
-    		if (shopItem.className == "addCart") {
-        		shopItem.className = "inCart";
-				shopItem.value = "In Your Cart";
-				$.post('scripts/addToCart.php', itemId); 
-    		} else if (shopItem.className = "inCart") {
-        		shopItem.className = "addCart";
-				shopItem.value = "Add To Cart";
-				$.post('scripts/removeCart.php', itemId); 
-			}
-		}
-
-		/* ___________Open Checkout____________ */
-		function openCheckout() {
-			window.open('checkout.php','_self',false)
-		}
-	
-	</script>
+	<script language="JavaScript" src="scripts/shop.js"></script>
 	
 	
 	
