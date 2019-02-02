3bbda1eb6afd57b6129e3edc6af710b5dde0cca3
amarro <abyhunter@gmail.com>
2019-02-02 12:32:11 -0700

    Starting Pursula Slider

--- web/pursula/scripts/slider.php
+++ web/pursula/scripts/slider.php
@@ -3,11 +3,11 @@
 	require('scripts/connectToDb.php'); 
 	$db = get_db(); 
 
-	foreach ($db->query('SELECT id, name, img_url, description FROM products WHERE description != ""') as $row)
-	{
-  		echo 'id: ' . $row['id'];
-  		echo ' name: ' . $row['name'];
-  		echo '<br/>';
-	}
+//	foreach ($db->query('SELECT id, name, img_url, description FROM products WHERE description != ""') as $row)
+//	{
+//  		echo 'id: ' . $row['id'];
+//  		echo ' name: ' . $row['name'];
+//  		echo '<br/>';
+//	}
 
 ?>
