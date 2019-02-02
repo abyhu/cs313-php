87104032992ca2ed0f434263b21d66a1e258e622
amarro <abyhunter@gmail.com>
2019-02-02 14:03:46 -0700

    Starting Pursula Slider

--- web/pursula/scripts/slider.php
+++ web/pursula/scripts/slider.php
@@ -3,22 +3,47 @@
 	require('scripts/connectToDb.php'); 
 	$db = get_db(); 
 
-	$data = $db->prepare("SELECT id, name, img_url, description FROM products WHERE description != ''"); 
+	$data = $db->prepare("SELECT name, description FROM products WHERE description != ''"); 
 	$data->execute(); 
 
-//	while ($row = $data->fetch(PDO::FETCH_ASSOC))
-//	{
-//		echo 'id: ' . $row['id'];
-//  		echo ' name: ' . $row['name'];
-//		echo 'img_url: ' . $row['img_url'];
-//  		echo 'description: ' . $row['description'];
-//  		echo '<br/>';
-//		echo '</p>';
-//	}
+	while ($row = $data->fetch(PDO::FETCH_ASSOC))
+	{
+		$ids[] = $row['id']; 
+		$names[] = $row['name']; 
+		$descriptions[] = $row['description']; 
+	}
 
-	echo $data[0]['name']; 
-	echo $data[0]['description']; 
-	
+	<script> 
+		/* ___________Slider Left/Right Arrows____________ */
+		function arrow(arrow) {
+			//establish variables to be used in the slider
+			var i = (document.getElementById("leftArrow").name);
+		
+			//the next item depends on which slider button was selected
+			if (arrow.value == "<") {
+				if (i == 0) {
+					i = 2;
+				} else {
+					i = i - 1;
+				}
+			} else {
+
+				if (i == 2) {
+					i = 0;
+				} else {
+					i++;
+				}
+			}
+			//change the slider items to the newly selected item
+			document.getElementById("leftArrow").setAttribute("name", i);
+			document.getElementById("sliderImage").setAttribute("src", "images/purse" + i + "large.jpg");
+			document.getElementById("sliderImage").setAttribute("alt", "Purse " + i);
+			//document.getElementById("sliderTitle").innerHTML = titles[i];
+			//document.getElementById("sliderText").innerHTML = texts[i];
+			//document.getElementById("buyNow").setAttribute("name", i);
+		}
+	</script>
+
 	
 
 
