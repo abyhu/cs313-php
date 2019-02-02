04fd9630ec296de1379b2029c326d846be62e085
amarro <abyhunter@gmail.com>
2019-02-02 14:04:34 -0700

    Starting Pursula Slider

--- web/pursula/scripts/slider.php
+++ web/pursula/scripts/slider.php
@@ -13,37 +13,7 @@
 		$descriptions[] = $row['description']; 
 	}
 
-	<script> 
-		/* ___________Slider Left/Right Arrows____________ */
-		function arrow(arrow) {
-			//establish variables to be used in the slider
-			var i = (document.getElementById("leftArrow").name);
-		
-			//the next item depends on which slider button was selected
-			if (arrow.value == "<") {
-				if (i == 0) {
-					i = 2;
-				} else {
-					i = i - 1;
-				}
-			} else {
-
-				if (i == 2) {
-					i = 0;
-				} else {
-					i++;
-				}
-			}
-			//change the slider items to the newly selected item
-			document.getElementById("leftArrow").setAttribute("name", i);
-			document.getElementById("sliderImage").setAttribute("src", "images/purse" + i + "large.jpg");
-			document.getElementById("sliderImage").setAttribute("alt", "Purse " + i);
-			//document.getElementById("sliderTitle").innerHTML = titles[i];
-			//document.getElementById("sliderText").innerHTML = texts[i];
-			//document.getElementById("buyNow").setAttribute("name", i);
-		}
-	</script>
-
+	
 	
 
 
