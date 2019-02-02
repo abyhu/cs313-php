5901669f81cff625544b2023579f0c3fc5a36fc8
amarro <abyhunter@gmail.com>
2019-02-02 13:43:13 -0700

    Starting Pursula Slider

--- web/pursula/scripts/slider.php
+++ web/pursula/scripts/slider.php
@@ -6,15 +6,20 @@
 	$data = $db->prepare("SELECT id, name, img_url, description FROM products WHERE description != ''"); 
 	$data->execute(); 
 
-	while ($row = $data->fetch(PDO::FETCH_ASSOC))
-	{
-		echo '<p>';
-  		echo 'id: ' . $row['id'];
-  		echo ' name: ' . $row['name'];
-		echo 'img_url: ' . $row['img_url'];
-  		echo 'description: ' . $row['description'];
-  		echo '<br/>';
-		echo '</p>';
-	}
+//	while ($row = $data->fetch(PDO::FETCH_ASSOC))
+//	{
+//		echo 'id: ' . $row['id'];
+//  		echo ' name: ' . $row['name'];
+//		echo 'img_url: ' . $row['img_url'];
+//  		echo 'description: ' . $row['description'];
+//  		echo '<br/>';
+//		echo '</p>';
+//	}
+
+	echo $data[0]['name']; 
+	echo $data[0]['description']; 
+	
+	
+
 
 ?>
