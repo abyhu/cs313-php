096633947d83f536fbd0e19b93bd32ba32b8db73
amarro <abyhunter@gmail.com>
2019-02-05 11:27:15 -0700

    Starting Shopping Page with Database Information

--- web/pursula/index.php
+++ web/pursula/index.php
@@ -34,14 +34,14 @@ while ($row = $data->fetch(PDO::FETCH_ASSOC)){
 
             <div id="index">
                 <div id="slider">
-                    <input type="button" id="leftArrow" value="<" onclick="arrow(this)" name="0" />
+                    <input type="button" id="leftArrow" value="<" onclick="arrow(this)" name="1" />
 					
 					<?php 
-					echo '<img src="images/purse'.$products[0][id].'large.jpg" alt="Purse '.$products[0][id].'" id="sliderImage" />';
+					echo '<img src="images/purse'.$products[0][id].'large.jpg" alt="Purse '.$products[1][id].'" id="sliderImage" />';
 					echo '<div id="sliderTextDiv">';
-						echo '<h2 id="sliderTitle">'.$products[0][name].'</h2>';
-						echo '<p id="sliderText">'.$products[0][description].'</p>';
-						echo '<input type="button" id="buyNow" name="'.$products[0][id].'" value="Buy Now" onclick="buyNow(this)" />';
+						echo '<h2 id="sliderTitle">'.$products[1][name].'</h2>';
+						echo '<p id="sliderText">'.$products[1][description].'</p>';
+						echo '<input type="button" id="buyNow" name="'.$products[1][id].'" value="Buy Now" onclick="buyNow(this)" />';
 					echo '</div>';
 					?>
 					
