ae64108876f2e93a7d276fb083fbd5348248c868
amarro <abyhunter@gmail.com>
2019-02-05 13:22:10 -0700

    Starting Shopping Page with Database Information

--- web/pursula/index.php
+++ web/pursula/index.php
@@ -40,8 +40,8 @@ while ($row = $data->fetch(PDO::FETCH_ASSOC)){
                     echo '<input type="button" id="leftArrow" value="<" onclick="arrow(this)" name="'.$_SESSION['i'].'" />'; 					
 					echo '<img src="images/purse'.$products[0][id].'large.jpg" alt="Purse '.$products[0][id].'" id="sliderImage" />';
 					echo '<div id="sliderTextDiv">';
-						echo '<h2 id="sliderTitle">'.$products[0]][name].'</h2>';
-						echo '<p id="sliderText">'.$products[0]][description].'</p>';
+						echo '<h2 id="sliderTitle">'.$products[0][name].'</h2>';
+						echo '<p id="sliderText">'.$products[0][description].'</p>';
 						echo '<input type="button" id="buyNow" name="'.$products[0][id].'" value="Buy Now" onclick="buyNow(this)" />';
 					echo '</div>';
 					?>
