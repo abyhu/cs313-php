dbec624c60861a8e605b9bf57484bb7e3cf1cd73
amarro <abyhunter@gmail.com>
2019-02-06 10:22:01 -0700

    Finishing Shopping Page and Slider with Database

--- web/pursula/shop.php
+++ web/pursula/shop.php
@@ -10,7 +10,7 @@ if(!isset($_SESSION['items'])) {
 require('scripts/connectToDb.php'); 
 	$db = get_db(); 
 
-	$data = $db->prepare("SELECT id, name, img_url, price FROM products"); 
+	$data = $db->prepare("SELECT id, name, img_url, price FROM products ORDER BY id"); 
 	$data->execute(); 
 ?>
 
