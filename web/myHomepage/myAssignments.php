4513030e4410a30309622c1a6a4f8e199d74291e
amarro <abyhunter@gmail.com>
2019-02-06 11:03:42 -0700

    Updating myHomepage with new aassignment

--- web/myHomepage/myAssignments.php
+++ web/myHomepage/myAssignments.php
@@ -14,7 +14,7 @@
 				 <p><em>CS 313 Web Engineering II:</em><br> According to the syllabus, "this course builds upon Web Engineering I allowing students to create more advanced web applications and services."</p> 
 				 <ul> 
 					 <li><a href="https://still-harbor-46445.herokuapp.com/shoppingCart/shop.php">PHP Shopping Cart</a></li>
-					 <li><a href="">Assignment Two ... Coming Soon!</a></li>
+					 <li><a href="https://still-harbor-46445.herokuapp.com/pursula/index.php">Database Access</a></li>
 					 <li><a href="">Assignment Three ... Coming Soon!</a></li>
 					 <li><a href="">Assignment Four ... Coming Soon!</a></li>
 					 <li><a href="">Assignment Five ... Coming Soon!</a></li>
