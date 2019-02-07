07e99e4df560f19e48182b60696b0b95b458c62e
amarro <abyhunter@gmail.com>
2019-02-07 11:12:36 -0700

    team06Practice

--- web/team06/insert.php
+++ web/team06/insert.php
@@ -49,14 +49,14 @@ $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
 $stmt->bindValue(':content', $content, PDO::PARAM_STR);
 $stmt->execute(); 
 
-foreach($topics[] as $topic) {
+foreach($topics as $topic) {
 	$topicId = $db->query('SELECT id from topic WHERE name = :name;'); 
-	$stmt->bindValue(':name', $topic, PDO::PARAM_STR); 
+	$stmt->bindValue(':name', htmlspecialchars($topic), PDO::PARAM_STR); 
 	$stmt->execute(); 
 	
 	$stmt = $db->prepare('INSERT INTO scripture_topic(scripture) VALUES (:scripture_id, :topic_id;'); 
 	$stmt->bindValue(':scripture_id', $scriptureId, PDO::PARAM_INT); 
-	$stmt->bindValue(':topic_id', $topic, PDO::PARAM_INT);
+	$stmt->bindValue(':topic_id', htmlspecialchars($topic), PDO::PARAM_INT);
 	$stmt->execute(); 
 }
   
