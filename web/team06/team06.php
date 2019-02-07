e777f3a205e737498f76e1ec41442d7e0265b139
amarro <abyhunter@gmail.com>
2019-02-07 10:07:56 -0700

    team06Practice

---
+++ web/team06/team06.php
@@ -0,0 +1,71 @@
+<?php
+
+$db;
+
+try
+{
+    $dbUrl = getenv('DATABASE_URL');
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+?>
+
+
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title>Team 06</title>
+        <style type="text/css">
+
+        </style>
+    </head>
+    <body>
+        <h1>Insert Scripture</h1>
+        <form method="post" action="insert.php">
+
+            <label for="book">Book</label>
+            <input name="book" id="book" type="text"/>
+            <br/><br/>
+			<label for="chapter">Chapter</label>
+            <input name="chapter" id="chapter" type="text"/>
+            <br/><br/>
+			<label for="verse">Verse</label>
+            <input name="verse" id="verse" type="text"/>
+            <br/><br/>
+			<label for="content">Content</label>
+            <input name="content" id="content" type="textarea"/>
+            <br/><br/>
+			
+			<?php
+			
+			foreach($db->query("SELECT * FROM topic;") as $row)
+            {
+                echo "<input type='checkbox' name='vehicle' value='".row[name].">".$row[name]."<br />";
+	        }
+			
+			?>
+			
+			
+			
+            <input  type="submit" value="Insert Scripture"/>
+
+        </form>
+
+    </body>
+</html>
