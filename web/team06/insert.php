15374b7f0fb3ba43e1db361599e86e01317b3681
amarro <abyhunter@gmail.com>
2019-02-07 15:32:12 -0700

    team06Practice

--- web/team06/insert.php
+++ web/team06/insert.php
@@ -37,28 +37,26 @@ $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
 $stmt->bindValue(':content', $content, PDO::PARAM_STR);
 $stmt->execute(); 
 
-$stmt = $db->query('SELECT id FROM scripture 
+$scriptureId = $db->prepare('SELECT id FROM scripture 
 WHERE book = :book AND chapter = :chapter AND verse = :verse 
 AND content = :content;'); 
-$stmt->bindValue(':book', $book, PDO::PARAM_STR); 
-$stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
-$stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
-$stmt->bindValue(':content', $content, PDO::PARAM_STR);
-$stmt->execute(); 
-
-$scriptureId = $stmt->fetch(PDO::FETCH_ASSOC); 
-
-//foreach($topics as $topic) {
-//	$topic = htmlentities($topic); 
-//	$topicId = $db->query('SELECT id FROM topic WHERE name = :name;'); 
-//	$stmt->bindValue(':name', $topic, PDO::PARAM_STR); 
-//	$stmt->execute(); 
-//	
-//	$stmt = $db->prepare('INSERT INTO scripture_topic(scripture) VALUES (:scripture_id, :topic_id);'); 
-//	$stmt->bindValue(':scripture_id', $scriptureId, PDO::PARAM_INT); 
-//	$stmt->bindValue(':topic_id', $topic, PDO::PARAM_INT);
-//	$stmt->execute(); 
-//}
+$scriptureId->bindValue(':book', $book, PDO::PARAM_STR); 
+$scriptureId->bindValue(':chapter', $chapter, PDO::PARAM_INT);
+$scriptureId->bindValue(':verse', $verse, PDO::PARAM_INT);
+$scriptureId->bindValue(':content', $content, PDO::PARAM_STR);
+$scriptureId->execute(); 
+ 
+foreach($topics as $topic) {
+	$topic = htmlentities($topic); 
+	$topicId = $db->prepare('SELECT id FROM topic WHERE name = :name;'); 
+	$stmt->bindValue(':name', $topic, PDO::PARAM_STR); 
+	$stmt->execute(); 
+	
+	$stmt = $db->prepare('INSERT INTO scripture_topic(scripture) VALUES (:scripture_id, :topic_id);'); 
+	$stmt->bindValue(':scripture_id', $scriptureId, PDO::PARAM_INT); 
+	$stmt->bindValue(':topic_id', $topic, PDO::PARAM_INT);
+	$stmt->execute(); 
+}
   
 
 ?>
