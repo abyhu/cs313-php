6304687772e979aca0d8509932ac290ad9343acb
amarro <abyhunter@gmail.com>
2019-02-13 20:45:25 -0700

    finishing DB modification

--- web/pursula2/shippingStatus.php
+++ web/pursula2/shippingStatus.php
@@ -15,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$phone = preventHacks($_POST["phone"]);
 	
 	$stmt = $db->prepare('SELECT id FROM customers WHERE last_name = ? AND first_name = ? AND street_address = ? AND city = ? AND state = ? AND zip = ? AND phone = ?');  
-	$stmt->execute($lName, $fName, $street, $city, $state, $zip, $phone);
+	$stmt->execute(array($lName, $fName, $street, $city, $state, $zip, $phone));
 
 	$customerId; 
 	while ($row = $stmt->fetch()) {
@@ -25,7 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	
 //	$stmt = $db->prepare('SELECT o.shipping_status, p.name, p.img_url FROM customers c INNER JOIN orders o ON c.id = o.customer_id INNER JOIN orders_products op ON o.id = op.order_id INNER JOIN products p ON op.product_id = p.id WHERE c.id = ?'); 
-//	$stmt->execute($customerId);
+//	$stmt->execute(array($customerId));
 //	
 //	//$orders[]; 
 //	
