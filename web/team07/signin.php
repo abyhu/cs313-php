0b0b5aa8af293ea235da337cad8c1268d1d6a5b7
amarro <abyhunter@gmail.com>
2019-02-16 09:01:18 -0700

    Starting team 07 activity

--- web/team07/signin.php
+++ web/team07/signin.php
@@ -1,5 +1,21 @@
 <?php
-
+if(isset($_POST['login'])){ //check if form was submitted
+  	$username = $_POST['username']; //get input text
+	$password = $_POST['password']; 
+	
+	//query database to make sure user exists
+	require 'connectToDb.php';
+	$db = get_db(); 
+	
+		
+	
+	//if user exists set session variable for the user 	
+  	if ()
+	
+	//redirect to the welcome page
+	header('Location: welcome.php');
+	die(); 
+}    
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +25,14 @@
     <title>Sign-In Page</title>
   </head>
   <body>
+	  <p>Please sign in: </p>
+	  <form method="post" action="">
+	  	Username: <input type="text" name="username"><br />
+		Password: <input type="text" name="password"><br />
+	  	<input type="submit" name="login" value="Login">
+	  </form>
+	  
+	  <p>If you do not have an account, create one <a href='signup.php'>here</a>.</p>
   
   </body>
 </html>
