e946cd349ce621391612be141e44bffd3fefa2f6
amarro <abyhunter@gmail.com>
2019-02-16 09:27:43 -0700

    Starting team 07 activity

--- web/team07/signin.php
+++ web/team07/signin.php
@@ -17,11 +17,11 @@ if(isset($_POST['login'])){ //check if form was submitted
 	require 'scripts/connectToDb.php';
 	$db = get_db(); 
 	
-	//$stmt = $pdo->prepare('SELECT user_id FROM authentication WHERE username = ? and password = ?'); 
-	//$stmt->execute(array($username, $password));
-	//$userId = $stmt->fetch(); 
+	$stmt = $pdo->prepare('SELECT user_id FROM authentication WHERE username = ? and password = ?'); 
+	$stmt->execute(array($username, $password));
+	$userId = $stmt->fetch(); 
 	
-	//echo $userId; 
+	echo $userId; 
 	
 	
 	
